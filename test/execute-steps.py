81fa7c857b9c7fc7cf0c48028be22071da5cb318
Christoph Burgmer <christoph@nwebs.de>
2011-08-19 23:05:09 +0200

    Fix test to work with new selenium release
    Currently testing needs bleeding edge of lettuce_webdriver which can be installed via
    pip install -e git+git://github.com/bbangert/lettuce_webdriver.git

--- test/execute-steps.py
+++ test/execute-steps.py
@@ -10,6 +10,6 @@ def then_i_see_spo_results(step):
 @step(u'Then I see SPO results')
 def then_i_see_spo_results(step):
     head = world.browser.find_element_by_xpath('//*[@id="query_results"]//table[@class="resulttable"]/thead')
-    assert_equals(head.text, 'spo')
+    assert_equals(head.text, 's p o')
     # Make sure we find more than the "None" entry
     return world.browser.find_element_by_xpath('//*[@id="query_results"]//table[@class="resulttable"]/tbody[count(tr)>1]')
